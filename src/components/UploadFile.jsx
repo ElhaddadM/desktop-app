8ab4fdf957c7d61999b36455d83634456b9799a0
ElhaddadM <Elhaddadtech@gmail.com>
2024-07-25 16:54:38 +0100

    WorkingLearnGrowth

--- src/components/UploadFile.jsx
+++ src/components/UploadFile.jsx
@@ -3,6 +3,7 @@ import Papa from 'papaparse';
 import { InboxOutlined } from '@ant-design/icons';
 import { message, Upload } from 'antd';
 import { StatisticsCard } from './StatisticsCard';
+import {exportToExcel} from '../app/(pages)/catalyst/Functions'
 import axios from 'axios'
 const { Dragger } = Upload;
 
@@ -70,7 +71,48 @@ const AddDataPlacement =   (file, table, data1) => {
  }
   axios.delete("http://localhost:3001/PlacementTestReport/f385").then(()=>{  StoreData() }).catch(()=>{ StoreData() })
 }
-
+const AddDataGrow =   (file, table, data1) => {
+  console.log("infoPlacement" , file , table , data1);
+  const data = []
+  const date = file.split(".")[1]
+  data1?.slice(1, (data1.length - 4)).map((element) => { 
+    if (element[10] !="" & element[15] !=""){
+          const obj =  { 
+          "Organization": element[0], 
+          "LastName": element[1], 
+          "FirstName": element[2], 
+          "Email": element[3], 
+          "Group": element[4], 
+          "Langue" : element[5],
+          "type" : element[14],
+          "Time" : element[12],
+          "Test1": element[16],
+          "Level1" : element[17],
+          "Test2": element[21], 
+          "Level2": element[22],
+          "Test3": element[26], 
+          "Level3": element[27],
+          "date": date 
+        }
+        data.push(obj)
+    }
+    
+  })
+  exportToExcel(data,table)
+  console.log("GrowD1" ,data1);
+  console.log("GrowD" ,data);
+ const StoreData = async ()=>{
+  try {
+    const resultObject = JSON.stringify({"id": "f385", data})
+    await axios.post(`http://localhost:3001/${table}`, resultObject)
+    message.success(`${file} file uploaded successfully.`);
+    console.log("Drop",table);
+  } catch (error) {
+    console.error(error)
+  }
+ }
+ axios.delete("http://localhost:3001/LearnerGrowthReport/f385").then(()=>{  StoreData() }).catch(()=>{ StoreData() })
+}
 const props = {
   name: 'file',
   multiple: false,
@@ -86,7 +128,7 @@ const props = {
           const fileName = info.file.name.split(".")
           if (fileName.includes(files[0]))   AddData(info.file.name, files[0], results.data) ;
           if (fileName.includes(files[1]))   AddDataPlacement(info.file.name, files[1], results.data) ; ;
-          if (fileName.includes(files[2]))   console.log('FileGrow' , results.data); ;
+          if (fileName.includes(files[2]))   AddDataGrow(info.file.name, files[2], results.data) ;
           // AddData(info.file.name, files[0], results.data)
         }
       });
