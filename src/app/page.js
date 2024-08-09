20aba94196bfeaf92efc0b5e00187cdf7b87c89c
ElhaddadM <Elhaddadtech@gmail.com>
2024-08-09 12:34:19 -0700

    add Button Home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Template from './Template'
-
+import Link from 'next/link'
 function Home() {
   return (
     <main>
-     LoginPage
+     LoginPage 
+     <div>
+       <Link href="/home" className='text-xl font-bold'>Home</Link>
+     </div>
     </main>
   )
 }
